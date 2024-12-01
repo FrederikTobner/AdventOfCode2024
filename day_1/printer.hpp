ac22acf286e79fe4c616148e17de1e59309587c4
Frederik Tobner <masterhorst90@web.de>
2024-12-01 18:37:34 +0100

    This ensures format strings are checked at compile-time

--- day_1/printer.hpp
+++ day_1/printer.hpp
@@ -1,20 +1,18 @@
 #pragma once
 
-// GCC 13 and clang do not have the <print> header yet, so we need to use the <format> header and define our own println
-// function. This function will be used to print formatted strings to the console or a file.
 #if defined(__GNUC__) && !defined(__clang__)
 #include <format>
 #include <iostream>
 
 namespace std {
-template <typename... Args> void println(const std::string_view fmt, Args &&... args) {
-    std::cout << std::format(fmt, std::forward<Args>(args)...) << '\n';
+template <typename... Args> void println(std::format_string<Args...> fmt, Args &&... args) {
+    std::cout << std::vformat(fmt.get(), std::make_format_args(args...)) << '\n';
 }
 
-template <typename... Args> void println(std::FILE * stream, const std::string_view fmt, Args &&... args) {
-    std::fprintf(stream, "%s\n", std::format(fmt, std::forward<Args>(args)...).c_str());
+template <typename... Args> void println(std::FILE * stream, std::format_string<Args...> fmt, Args &&... args) {
+    std::fprintf(stream, "%s\n", std::vformat(fmt.get(), std::make_format_args(args...)).c_str());
 }
 } // namespace std
-#else // MSVC already supports the <print> header
+#else
 #include <print>
 #endif
